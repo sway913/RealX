0f69125c0a2bb8c35d9470a07f44ee2e2608f5ad
ouyangshaocong <ouyangshaocong@yy.com>
2018-11-15 20:37:33 +0800

    极光分享

---
+++ app/src/main/java/com/yy/realx/RealXApplication.java
@@ -0,0 +1,12 @@
+package com.yy.realx;
+
+import android.support.multidex.MultiDexApplication;
+import cn.jiguang.share.android.api.JShareInterface;
+
+public class RealXApplication extends MultiDexApplication {
+    @Override
+    public void onCreate() {
+        super.onCreate();
+        JShareInterface.init(this);
+    }
+}
