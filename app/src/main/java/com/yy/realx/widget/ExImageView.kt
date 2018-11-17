d5014cdee132e467e009e02340b9c3aa3a319d98
ouyangshaocong <ouyangshaocong@yy.com>
2018-11-17 14:04:07 +0800

    1、编辑点

--- app/src/main/java/com/yy/realx/widget/ExImageView.kt
+++ app/src/main/java/com/yy/realx/widget/ExImageView.kt
@@ -1,24 +1,29 @@
 package com.yy.realx.widget
 
 import android.content.Context
-import android.graphics.Canvas
-import android.graphics.Color
-import android.graphics.Paint
+import android.graphics.*
 import android.util.AttributeSet
 import android.util.Log
+import android.view.GestureDetector
+import android.view.MotionEvent
+import android.view.ScaleGestureDetector
+import java.util.concurrent.atomic.AtomicBoolean
+
 
 class ExImageView @JvmOverloads constructor(context: Context, attrs: AttributeSet? = null, defStyleAttr: Int = 0) :
     RImageView(context, attrs, defStyleAttr) {
 
     companion object {
         private val TAG = ExImageView::class.java.simpleName
+        const val MIN_SCALE: Float = 1F
+        const val MAX_SCALE: Float = 5F
     }
 
     private val paint: Paint = Paint(Paint.ANTI_ALIAS_FLAG)
 
     init {
         //约束图片缩放模式
-        scaleType = ScaleType.CENTER_INSIDE
+        scaleType = ScaleType.MATRIX
         //初始化变量
         paint.style = Paint.Style.FILL_AND_STROKE
         paint.color = Color.LTGRAY
@@ -40,7 +45,8 @@ class ExImageView @JvmOverloads constructor(context: Context, attrs: AttributeSe
         }
     }
 
-    private var coordinates = mutableListOf<Float>()
+    private val coordinates = mutableListOf<Float>()
+    private val points = mutableListOf<PointF>()
 
     /**
      * 拆解坐标数据
@@ -58,21 +64,218 @@ class ExImageView @JvmOverloads constructor(context: Context, attrs: AttributeSe
             coordinates.clear()
             coordinates.addAll(values)
         }
+        points.clear()
         Log.d(TAG, "parseCoordinates():${coordinates.size}")
+        val scale = getNowScale()
+        val translate = getNowTranslate()
+        var point = PointF()
         values.mapIndexed { index, value ->
             if (index % 2 == 0) {
-                coordinates[index] = value * width
+                point = PointF()
+                coordinates[index] = value * width * scale + translate.x
+                point.x = coordinates[index]
             } else {
-                coordinates[index] = value * height
+                coordinates[index] = value * height * scale + translate.y
+                point.y = coordinates[index]
+                points.add(point)
+            }
+        }
+    }
+
+    /**
+     * 重新设定值
+     */
+    private fun remarkBackward() {
+        val scale = getNowScale()
+        val translate = getNowTranslate()
+        var point: PointF
+        coordinates.mapIndexed { index, value ->
+            point = points[index / 2]
+            if (index % 2 == 0) {
+                values[index] = (value - translate.x) / (scale * width)
+                point.x = value
+            } else {
+                values[index] = (value - translate.y) / (scale * height)
+                point.y = value
             }
         }
     }
 
     override fun setScaleType(scaleType: ScaleType?) {
-        if (scaleType != ScaleType.CENTER_INSIDE) {
-            throw IllegalArgumentException("Only support scaleType ScaleType.CENTER_INSIDE.")
+        if (scaleType != ScaleType.MATRIX) {
+            throw IllegalArgumentException("Only support scaleType ScaleType.MATRIX.")
         }
         super.setScaleType(scaleType)
+    }
+
+    private val _matrix = Matrix()
+
+    /**
+     * 返回当前缩放大小
+     */
+    private fun getNowScale(): Float {
+        Log.d(TAG, "getNowScale()")
+        val values = FloatArray(9)
+        _matrix.getValues(values)
+        return values[Matrix.MSCALE_X]
+    }
+
+    /**
+     * 返回当前移动位置
+     */
+    private fun getNowTranslate(): PointF {
+        Log.d(TAG, "getNowTranslate()")
+        val values = FloatArray(9)
+        _matrix.getValues(values)
+        return PointF(values[Matrix.MTRANS_X], values[Matrix.MTRANS_Y])
+    }
+
+    private val single: GestureDetector = GestureDetector(context, object :
+        GestureDetector.SimpleOnGestureListener() {
+        override fun onDoubleTap(event: MotionEvent?): Boolean {
+            Log.d(TAG, "onDoubleTap()")
+            if (null != event) {
+                restrictScale(getNowScale() * 1.5f, event.x, event.y)
+            }
+            return true
+        }
+
+        override fun onScroll(first: MotionEvent?, second: MotionEvent?, distanceX: Float, distanceY: Float): Boolean {
+            Log.d(TAG, "onScroll():$distanceX, $distanceY")
+            if (first?.pointerCount ?: 0 > 1 || second?.pointerCount ?: 0 > 1) {
+                return false
+            }
+            if (markCoordinate(first, second, distanceX, distanceY)) {
+                this@ExImageView.postInvalidate()
+            } else if (!remark.get()) {
+                restrictTranslate(-distanceX, -distanceY)
+            }
+            return true
+        }
+    })
+
+    private val remark = AtomicBoolean(false)
+
+    /**
+     * 标记是否触摸点是检测点
+     */
+    private fun markCoordinate(
+        first: MotionEvent?,
+        second: MotionEvent?,
+        distanceX: Float,
+        distanceY: Float
+    ): Boolean {
+        Log.d(TAG, "markCoordinate():${first?.x}, ${first?.y}, ${second?.x}, ${second?.y}")
+        if (null == first || null == second) {
+            return false
+        }
+        if (coordinates.isNullOrEmpty()) {
+            return false
+        }
+        val scale = getNowScale()
+        Log.d(TAG, "markCoordinate():${coordinates[0]}, ${coordinates[1]}")
+        val x = first.x
+        val y = first.y
+        var index = -1
+        points.mapIndexed { idx, point ->
+            if (Math.abs(point.x - x) < 5 * scale && Math.abs(point.y - y) < 5 * scale) {
+                index = idx
+                return@mapIndexed
+            }
+        }
+        Log.d(TAG, "markCoordinate():${points.size}, $index")
+        if (index < 0 || index > points.size) {
+            return false
+        }
+        remark.set(true)
+        coordinates[index * 2] -= distanceX
+        coordinates[index * 2 + 1] -= distanceY
+        return true
+    }
+
+    /**
+     * 获取图片显示的rect
+     */
+    private fun getMatrixRect(): RectF {
+        Log.d(TAG, "getMatrixRect()")
+        val rect = RectF()
+        val d = drawable
+        if (d != null) {
+            rect.set(0f, 0f, d.intrinsicWidth.toFloat(), d.intrinsicHeight.toFloat())
+            _matrix.mapRect(rect)
+        }
+        return rect
+    }
+
+    /**
+     * 左右滑动
+     */
+    private fun restrictTranslate(x: Float, y: Float) {
+        Log.d(TAG, "restrictTranslate()")
+        _matrix.postTranslate(x, y)
+        val rect = getMatrixRect()
+        var deltaX = 0f
+        var deltaY = 0f
+        if (rect.top > 0) {
+            deltaY = -rect.top
+        }
+        if (rect.bottom < height) {
+            deltaY = height - rect.bottom
+        }
+        if (rect.left > 0) {
+            deltaX = -rect.left
+        }
+        if (rect.right < width) {
+            deltaX = width - rect.right
+        }
+        _matrix.postTranslate(deltaX, deltaY)
+        imageMatrix = _matrix
+        //重新标记点
+        coordinates.clear()
+    }
+
+    private val multiple = ScaleGestureDetector(context, object :
+        ScaleGestureDetector.SimpleOnScaleGestureListener() {
+        override fun onScale(detector: ScaleGestureDetector?): Boolean {
+            Log.d(TAG, "onScale():${detector?.focusX}, ${detector?.focusY}")
+            if (null != detector) {
+                val translate = getNowTranslate()
+                restrictScale(getNowScale() * detector.scaleFactor, detector.focusX, detector.focusY)
+            }
+            return true
+        }
+    })
+
+    /**
+     * 约束缩放比例
+     */
+    private fun restrictScale(s: Float, x: Float, y: Float) {
+        val scale = Math.max(Math.min(s, MAX_SCALE), MIN_SCALE)
+        Log.d(TAG, "restrictScale():$s -> $scale")
+        _matrix.setScale(scale, scale, x, y)
+        imageMatrix = _matrix
+        //重新标记点
+        coordinates.clear()
+    }
+
+    override fun onTouchEvent(event: MotionEvent?): Boolean {
+        if (null == event) {
+            return false
+        }
+        val action = event.action
+        if (action == MotionEvent.ACTION_UP
+            || action == MotionEvent.ACTION_CANCEL
+        ) {
+            Log.d(TAG, "onTouchEvent():${remark.get()}")
+            if (remark.get()) {
+                remarkBackward()
+            }
+            remark.set(false)
+        }
+        Log.d(TAG, "onTouchEvent():${event.pointerCount}")
+        if (single.onTouchEvent(event)) return true
+        multiple.onTouchEvent(event)
+        return true
     }
 
     override fun onDraw(canvas: Canvas) {
@@ -80,6 +283,7 @@ class ExImageView @JvmOverloads constructor(context: Context, attrs: AttributeSe
         Log.d(TAG, "onDraw():${coordinates.isEmpty()}, ${values.isNotEmpty()}")
         if (coordinates.isNotEmpty()) {
             canvas.save()
+            paint.strokeWidth = 10 * getNowScale()
             canvas.drawPoints(coordinates.toFloatArray(), paint)
             canvas.restore()
         } else if (values.isNotEmpty()) {
